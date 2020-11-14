7e75725d922fa513201f443afa67aad0c6120c8a
sumyaraha <61174012+sumyaraha@users.noreply.github.com>
2020-11-13 20:30:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const cost_input = document.querySelector('.cost');
+const service_input = document.querySelector('.service');
+const people_input = document.querySelector('.people');
+
+const tip_btn = document.querySelector('#tip-btn');
+
+tip_btn.addEventListener('click', calc_tip);
+
+function calc_tip () {
+ let cost = Number(cost_input.value);
+ let service = Number(service_input.value) / 100;
+ let people = Number(people_input.value);
+ let tip = (cost * service) / people;
+ 
+ let tip_el = document.querySelector('.tip-wrap .tip');
+ tip_el.innerText = '£' + tip.toFixed(2) + ((people > 1) ? ' each' : '');
+}
